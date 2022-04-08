bd494925d82d2a77063778e4735ecb18312c59a7
Dj queen <dqueen1223@gmail.com>
2022-04-07 20:02:33 -0400

    create bot

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,33 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const HomePage = () => (
-  <>
-    <div id="patientImgDiv">
+/**
+ * @name ChatBot
+ * @description displays chatbot and its messages.
+ * @returns component
+ */
+const Chatbot = () => {
+  const [lastOption, setLastOption] = useState([]);
+  const [previousOption, setPreviousOption] = useState([]);
+  const [option, setOption] = useState([]);
+
+  const handleClick = (Choice) => {
+    setPreviousOption(option);
+    setOption(Choice);
+  };
+
+  return (
+    <div className="chatbox">
+      <p>
+        Hello user, how may I assist you today?
+      </p>
+      {(option === 'About Me?' || previousOption === 'About Me?') && (
       <div>
-        <p id="headerText">
-          Welcome to Dr. Record Assist
-          <br />
+        <p className="user">
+          About you?
         </p>
-        <p id="homeText">
-          Where providers can manage their patients and records
-          with ease.
+        <p>
+          I knew the moment I solved my first problem that programming was for me, solving difficult
+          problems with a team of people is very inspiring and something I am proud of accomplishing
+          .
           <br />
-          Click the patient icon in the upper right corner to view and update the patient table.
-          <br />
-          <br />
-          (hint: hovering over each icon tells you what it does.)
+          I am eager to learn much more about programming and work on many other projects.
         </p>
       </div>
-      <p id="contactText">
-        Created by: Daryl Queen
-        <br />
-        Contact: dqueen1223@gmail.com
-      </p>
-      <img src="https://media.istockphoto.com/photos/healthcare-business-concept-medical-examination-and-growth-graph-data-picture-id1274428125?b=1&k=20&m=1274428125&s=612x612&w=0&h=taMkWIr1WNlJIUzlphGaWVvK2QFXH1dqEJ6pANj44UY=" alt="Medical records and a laptop" id="patientImg" />
+      )}
+      {(option === 'I want to talk to the bot' || previousOption === 'I want to talk to the bot') && (
+      <>
+        <>
+          <p className="user">I want to talk to the bot</p>
+          <p>
+            Beep Boop beepidy boop.
+          </p>
+        </>
+        {option === 'I want to talk to the bot' && (
+        <>
+          <button onClick={() => handleClick('About Alfred?')} type="button">About Alfred?</button>
+        </>
+        )}
+      </>
+      )}
+      {(option === 'About Alfred?' || previousOption === 'About Alfred?') && (
+      <div>
+        <p className="user">
+          About Alfred?
+        </p>
+        <p>
+          Alfred was a bot designed by me, sadly all he can say is beep boop.
+        </p>
+      </div>
+      )}
+      {option === 'Skills' && (
+        <>
+          <p className="user">What are your skills?</p>
+          <p>
+            I am currently adept in C#, and Javascript using React and ASP.Net, I am always learning
+            more about them and plan to learn Java and Python.
+            I am also familiar with scrum techniques, as I have worked with them in the past.
+          </p>
+        </>
+      )}
+      {lastOption === 'Alfreds Skills?' && (
+      <>
+        <p className="user">Alfreds Skills?</p>
+        <p>
+          01010011 01100001 01100100 01101100 01111001 00100000 01101110 01101111 00100000
+          01101111 01101110 01100101 00100000 01101011 01101110 01101111 01110111 01110011
+          00100000 01110111 01101000 01100001 01110100 00100000 01001001 00100111 01101101
+          00100000 01110011 01100001 01111001 01101001 01101110 01100111
+        </p>
+      </>
+      )}
+      {option.length === 0 && (
+      <>
+        <button onClick={() => setOption('About Me?')} type="button">About Me?</button>
+        <button onClick={() => setOption('I want to talk to the bot')} type="button">I want to talk to the bot</button>
+      </>
+      )}
+      {option === 'About Me?' && (<button onClick={() => handleClick('Skills')} type="button">Skills</button>)}
+      {option === 'About Alfred?' && lastOption.length === 0 && (<button onClick={() => { setLastOption('Alfreds Skills?'); }} type="button">Alfreds Skills?</button>)}
     </div>
-    <div>
-      <img src="https://thumbs.dreamstime.com/b/medicine-doctor-touching-electronic-medical-record-tablet-dna-digital-healthcare-network-connection-hologram-modern-virtual-154742526.jpg" alt="Doctor with electronic record" id="doctorImg" />
-    </div>
-  </>
-);
-export default HomePage;
+  );
+};
+
+export default Chatbot;
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import PatientsPage from '../PatientsPage/PatientsPage';
 import Header from '../header/Header';
-import PatientDetailsPage from '../PatientsPage/patientDetailsPage';
 import NotFoundPage from '../notFound';
-import HomePage from '../Home/HomePage';
-import CreatePatientPage from '../PatientsPage/createPatientPage';
-import CreateEncountersPage from '../encounters/createEncountersPage';
-import EncounterDetailsPage from '../encounters/encounterDetailsPage';
+import Chatbot from '../Home/HomePage';
 
 toast.configure();
 /**
@@ -27,23 +22,7 @@ const App = () => (
       />
     </div>
     <Switch>
-      <Route exact path="/" render={() => <HomePage />} />
-      <Route exact path="/patients" render={() => <PatientsPage />} />
-
-      <Route
-        exact
-        path="/patients/create"
-        render={() => <CreatePatientPage />}
-      />
-      <Route path="/patients/details/:id">
-        <PatientDetailsPage />
-      </Route>
-      <Route path="/patients/encounters/details/:id">
-        <EncounterDetailsPage />
-      </Route>
-      <Route path="/patients/encounters/create/:id">
-        <CreateEncountersPage />
-      </Route>
+      <Route exact path="/" render={() => <Chatbot />} />
       <Route path="*" render={NotFoundPage} />
     </Switch>
   </BrowserRouter>
